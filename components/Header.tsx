8aa6621bcc317cf22adcfa60b3e00dec856a952d
cjuyematsu <cjuyematsu@gmail.com>
2025-06-12 18:21:09 -0700

    logo added

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,8 @@ import { PlayerSuggestion } from '@/types/stats';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link'; 
 import HeaderSearchBar from '@/components/HeaderSearchBar';
-import { useAuth } from '../app/contexts/AuthContext'; 
+import { useAuth } from '../app/contexts/AuthContext';
+import Image from 'next/image' 
 
 export default function Header() {
   const router = useRouter();
@@ -55,6 +56,22 @@ export default function Header() {
       className={headerClasses}
     >
       <div className={`flex items-center justify-between h-[var(--header-height)] ${contentPaddingLeft} pr-4`}>
+      <div className="flex items-center flex-shrink-0">
+            <div className="flex items-center">
+                <Image
+                    src="/logo2.png" // Assumes logo is in the /public folder
+                    alt="Site Logo"
+                    width={40}
+                    height={40}
+                    className="h-8 w-8 sm:h-10 sm:w-10"
+                    priority
+                />
+                <span className="ml-2 mr-5 text-xl font-bold tracking-tight text-slate-800 dark:text-slate-100">
+                    stats-leader
+                </span>
+            </div>
+        </div>
+
         <div className="flex-grow min-w-0"> 
           <HeaderSearchBar onPlayerSelected={handlePlayerSelection} />
         </div>
