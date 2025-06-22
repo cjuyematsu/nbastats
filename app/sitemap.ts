35b9fdf74f03abaafcbd9d65228783ca44009a26
cjuyematsu <cjuyematsu@gmail.com>
2025-06-22 15:25:37 -0700

    more pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,11 +19,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+        url: `${baseUrl}/compare`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+    {
       url: `${baseUrl}/degrees-of-separation`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+        url: `${baseUrl}/analysis/growth-of-nba`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.7,
+      },
+      {
+        url: `${baseUrl}/analysis/salary-vs-points`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.7,
+      },
     {
       url: `${baseUrl}/games/stat-over-under`,
       lastModified: new Date(),
